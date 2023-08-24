a18d88af07f33642484f9fa36fbe5458f8b82e59
Igor Sorokin <ig40in@gmail.com>
2023-08-24 02:11:41 -0400

    extended translator and it's test case

--- translation/translator.go
+++ translation/translator.go
@@ -1,5 +1,30 @@
 package translation
 
+import "strings"
+
+func sanitizeInput(w string) string {
+  w = strings.TrimSpace(w)
+  return strings.ToLower(w)
+}
+
 func Translate(word string, language string) string {
-  return word
+  word = sanitizeInput(word)
+  language = sanitizeInput(language)
+
+  // handling unknown words
+  if word != "hello" {
+    return ""
+  }
+
+  // handling language
+  switch language {
+  case "english":
+    return "hello"
+  case "finnish":
+    return "hei"
+  case "german":
+    return "hallo"
+  default:
+    return ""
+  }
 }
--- translation/translator_test.go
+++ translation/translator_test.go
@@ -7,14 +7,67 @@ import (
 
 func TestTranslate(t *testing.T) {
   // Arrange
-  word := "hello"
-  language := "english"
-
-  // Act
-  res := translation.Translate(word, language)
-
-  // Assert
-  if res != "hello" {
-    t.Errorf(`expected "%s" but recieved "%s"`, word, res)
+  tt := []struct {
+    Word         string
+    Language     string
+    Translation  string
+  }{
+    {
+      Word:         "hello",
+      Language:     "english",
+      Translation:  "hello",
+    },
+    {
+      Word:         "hello",
+      Language:     "german",
+      Translation:  "hallo",
+    },
+    {
+      Word:         "hello",
+      Language:     "finnish",
+      Translation:  "hei",
+    },
+    {
+      Word:         "hello",
+      Language:     "dutch",
+      Translation:  "",
+    },
+    {
+      Word:         "bye",
+      Language:     "dutch",
+      Translation:  "",
+    },
+    {
+      Word:         "bye",
+      Language:     "german",
+      Translation:  "",
+    },
+    {
+      Word:         "hello",
+      Language:     "German",
+      Translation:  "hallo",
+    },
+    {
+      Word:         "Hello",
+      Language:     "german",
+      Translation:  "hallo",
+    },
+    {
+      Word:         "hello ",
+      Language:     "german",
+      Translation:  "hallo",
+    },
   }
+
+  for _, test := range tt {
+    // Act
+    res := translation.Translate(test.Word, test.Language)
+
+    // Assert
+    if res != test.Translation {
+      t.Errorf(`expected "%s" to be "%s" from "%s" but received "%s"`,
+      test.Translation, test.Word, test.Language, res)
+    }
+  }
+
 }
